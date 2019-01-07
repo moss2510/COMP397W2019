775336a0aab4d16631950869ce839d064f7a4054
moss2510 <31931251+moss2510@users.noreply.github.com>
2019-01-07 18:17:10 -0500

    Added NPM support

---
+++ Scripts/core/game.js
@@ -0,0 +1,15 @@
+//IIFE - Immediate Invoked Function Expression
+/*
+closure
+calls anonymous self executing function
+*/
+(function(){
+
+    function Init(){
+
+        console.log("Init start");
+    }
+window.onload = Init;
+
+
+})();
